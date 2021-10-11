37eca84f51bc1dfb330eb6e9c57c359500938926
ManpreetRajpal <mrajpal@thoughtworks.com>
2021-10-11 20:55:28 +0530

    updated Get method in client.js

--- util/client.js
+++ util/client.js
@@ -7,16 +7,14 @@ var commonHeaders = {
 
 exports.GET = async ({endpoint, baseUrl = "http://localhost:3000/", headers = commonHeaders}) => {
 
-    if (headers!=undefined){
-        headers={...commonHeaders,...headers};
-    }
-    else {
-        headers=commonHeaders;
+    if (headers != undefined) {
+        headers = {...commonHeaders, ...headers};
+    } else {
+        headers = commonHeaders;
     }
 
     try {
         let response = await supertest(baseUrl).get(endpoint)
-            .set(commonHeaders)
             .set(headers);
         return response;
     } catch (error) {
