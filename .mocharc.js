97d412f411dd672f6a039d7d88a4dae76d0db85a
ManpreetRajpal <mrajpal@thoughtworks.com>
2021-10-21 23:18:38 +0530

    rename setup/local to setup/env_local

--- .mocharc.js
+++ .mocharc.js
@@ -6,7 +6,7 @@ module.exports = {
   package: "./package.json",
   reporter: "mochawesome",
   timeout: 5000,
-  require: ["@babel/register", "setup/" + env + ".js"],
+  require: ["@babel/register", "setup/env_" + env + ".js"],
   retries: 1,
   spec: ["test/spec/*.js"],
 };
